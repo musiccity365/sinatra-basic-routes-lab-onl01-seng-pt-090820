d5c41c2b5c7ca97fa91275f2270fdf2e616b65d5
musiccity365 <m_sackstein@yahoo.com>
2020-12-10 19:13:06 -0500

    commit tree fdc74c29cb996a5222950a818168862e87fcc3fc

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Michael"
+    end
+
+    get '/hometown' do
+        "My hometown is Arlington"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Imagine"
+    end
 end
